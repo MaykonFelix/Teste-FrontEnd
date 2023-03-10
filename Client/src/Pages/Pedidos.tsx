fabd563fc3d75327071048e4cf9d8d644ace9d2e
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-03-10 14:26:50 -0300

    Update

---
+++ Client/src/Pages/Pedidos.tsx
@@ -0,0 +1,120 @@
+import { useEffect, useState } from "react";
+import api from "../components/axios";
+
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+import './Pedidos.css'
+
+
+const steps = [
+    'Pedido',
+    'Preparando',
+    'Finalizado',
+];
+
+function Pedidos() {
+
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async () => {
+        try {
+            const response = await api.get('/api/pedido');
+            var data = response.data;
+            setPosts(data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+
+
+    return (
+        <>
+            <div className="bg-slate-100 w-screen h-screen flex flex-col justify-center items-center">
+
+                <div className="shadow-lg shadow-sky-500/30 hover:bg-sky-200/20 duration-500 text-white w-[35rem] h-screen rounded-lg m-2 justify-center items-center flex flex-col">
+
+                    <Box sx={{ width: '60%' }}>
+                        <Stepper activeStep={0} alternativeLabel>
+                            {steps.map((label) => (
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                </Step>
+                            ))}
+                        </Stepper>
+                    </Box>
+
+                    <div className="home flex text-center flex-col gap-2 p-12">
+
+                        <h1 className="uppercase tracking-widest text-slate-500">Escolha a Fruta:</h1>
+                        <div className="flex justify-center mb-2">
+                            {posts.length === 0 ?
+                                (<p>Carregando..</p>) :
+                                (posts.fruta.map((post, index) => (
+                                    <div key={index}>
+                                        <p className="bg-pink-500 m-2 w-[6rem] h-[3rem]  hover:bg-pink-600 cursor-pointer flex items-center justify-center rounded-xl" >{post.fruta}</p>
+                                    </div>
+                                )))}
+                        </div>
+
+                        <h1 className="uppercase tracking-widest text-slate-500">Recheio:</h1>
+                        <div className="flex justify-center mb-2">
+                            {posts.length === 0 ?
+                                (<p>Carregando..</p>) :
+                                (posts.complemento.map((post, index) => (
+                                    <div key={index}>
+                                        <p className="bg-pink-500 m-2 w-[6rem] h-[3rem]  hover:bg-pink-600 cursor-pointer flex items-center justify-center rounded-xl" >{post.complemento}</p>
+                                    </div>
+
+                                )))}
+                        </div>
+
+                        <h1 className="uppercase tracking-widest text-slate-500">Tamanho:</h1>
+                        <div className="flex justify-center mb-2 ">
+                            {posts.length === 0 ?
+                                (<p>Carregando..</p>) :
+                                (posts.tamanho.map((post, index) => (
+                                    <div className="bg-pink-500 m-2 w-[6rem] h-[3rem]  hover:bg-pink-600 cursor-pointer flex flex-col items-center justify-center rounded-xl" key={index}>
+                                        <p className="" >{post.tamanho}</p>
+                                        <span className=" after:content-[',00'] before:content-['R$']"> {post.price}</span>
+                                    </div>
+                                )))}
+
+                        </div>
+
+                        <form>
+                            <div className="flex gap-4 justify-around ">
+
+                                <label className="btn1 w-[6rem] hover:bg-pink-500 rounded-xl text-sky-500 hover:text-white flex flex-col align-center">
+                                    <input name="origin" type="radio" value=",morango" hidden /><p className="h-[3rem] flex flex-col justify-center">Morango</p>
+                                </label>
+
+                                <label className="btn1 w-[6rem] hover:bg-pink-500 rounded-xl text-sky-500 hover:text-white flex flex-col align-center">
+                                    <input name="origin" type="radio" value="banana" hidden /><p className="h-[3rem] flex flex-col justify-center">Banana</p>
+                                </label>
+
+                                <label className="btn1 w-[6rem] hover:bg-pink-500 rounded-xl text-sky-500 hover:text-white flex flex-col align-center">
+                                    <input name="origin" type="radio" value="kiwi" hidden /><p className="h-[3rem] flex flex-col justify-center">Kiwi</p>
+                                </label>
+
+                            </div>
+                        </form>
+
+                        <div className="flex m-2 p-2 gap-4 justify-center">
+                            <button className="bg-sky-500 w-[8rem] h-[2.5rem] rounded-xl hover:text-sky-500 hover:bg-white text-slate-500 text-white">Realizar Pedido</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Pedidos;
