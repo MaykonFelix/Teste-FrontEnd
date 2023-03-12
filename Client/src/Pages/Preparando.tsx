7d33ead80b1c656d729454d5303d6a2ee9c9114a
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-03-12 16:16:35 -0300

    Update

--- Client/src/Pages/Preparando.tsx
+++ Client/src/Pages/Preparando.tsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 
+// Tipagem Inicial
+/* interface IshoppingCartFruitItem {
+    produto: IFruits
+    quantidade: number
+}; */
+
+
 interface IFruits {
     id: number,
     name: string,
@@ -11,15 +18,17 @@ interface Icomplement {
     id: number,
     name: string,
 };
-interface IFruits {
+interface IsizeCup {
     id: number,
     name: string,
+    price: number,
 };
 
-
-interface IShoppingCartItem {
+interface IshoppingCartFruitItem {
     produto: IFruits
-    quantidade: number
+};
+interface shoppingCartComplement {
+    produto: IFruits
 };
 
 const fruits: IFruits[] = [
@@ -28,70 +37,134 @@ const fruits: IFruits[] = [
     { id: 3, name: "Kiwi" }
 ];
 
-const complement: IFruits[] = [
+const complements: Icomplement[] = [
     { id: 1, name: "Granola" },
     { id: 2, name: "Paçoca" },
     { id: 3, name: "Leite Ninho" }
 ];
 
-const sizeCup: IFruits[] = [
-    { id: 1, name: "Morango", price: 10 },
-    { id: 2, name: "Banana", price: 12 },
-    { id: 3, name: "Kiwi", price: 12 }
+const sizeCups: IsizeCup[] = [
+    { id: 1, name: "Pequeno", price: 10 },
+    { id: 2, name: "Médio", price: 12 },
+    { id: 3, name: "Grande", price: 12 }
 ];
 
 
 export default function Preparando() {
-
-    const [shoppingCart, setShoppingCart] = useState<IShoppingCartItem[]>([]);
-
-    const handleAddToCart = (id: number) => {
-        const fruit = fruits.find(fruit => fruit.id === id);
-
-        // --- Abaixo Confere se é repetido o Item
-        const alreadyInShoppingCart = shoppingCart.find(item => item.produto.id === id);
-
-        if (alreadyInShoppingCart) {
-            const newShoppingCart: IShoppingCartItem[] = shoppingCart.map(item => {
-                if (item.produto.id === id) ({
-                    ...item,
-                    quantidade: item.quantidade++,
+    // Função inicial
+    /* 
+        const [shoppingCartFruit, setshoppingCartFruit] = useState<IshoppingCartFruitItem[]>([]);
+    
+        const handleAddToCart = (id: number) => {
+            // --- Procura pelo id a fruta escolhida
+            const fruit = fruits.find(fruit => fruit.id === id);
+            // --- Abaixo Confere se é repetido o Item
+            const alreadyInshoppingCartFruit = shoppingCartFruit.find(item => item.produto.id === id);
+            // --- Se for repetida adiciona mais um na quantidade
+            if (alreadyInshoppingCartFruit) {
+                const newshoppingCartFruit: IshoppingCartFruitItem[] = shoppingCartFruit.map(item => {
+                    if (item.produto.id === id) ({
+                        ...item,
+                        quantidade: item.quantidade++,
+                    });
+                    return item;
                 });
-                return item;
-            });
-            setShoppingCart(newShoppingCart);
-            return;
+                setshoppingCartFruit(newshoppingCartFruit);
+                return;
+            };
+            // Adicionar o Item
+            const cartItem: IshoppingCartFruitItem = {
+                produto: fruit!,
+                quantidade: 1,
+            };
+            const newshoppingCartFruit: IshoppingCartFruitItem[] = [...shoppingCartFruit, cartItem];
+            setshoppingCartFruit(newshoppingCartFruit);
         };
+    
+        const handleRemoveToCart = (id: number) => {
+            const alreadyInshoppingCartFruit = shoppingCartFruit.find(item => item.produto.id === id);
+    
+            if (alreadyInshoppingCartFruit!.quantidade > 1) {
+                const newshoppingCartFruit: IshoppingCartFruitItem[] = shoppingCartFruit.map(item => {
+                    if (item.produto.id === id) ({
+                        ...item,
+                        quantidade: item.quantidade--,
+                    });
+                    return item;
+                });
+                setshoppingCartFruit(newshoppingCartFruit);
+                return;
+            };
+    
+            // if there is only one item with the id in the cart
+            const newshoppingCartFruit: IshoppingCartFruitItem[] = shoppingCartFruit.filter(item => item.produto.id !== id);
+            setshoppingCartFruit(newshoppingCartFruit);
+    
+        };
+     */
 
-        // if fruit is not already in the shopping cart --- Abaixo Adicionar o Item
-        const cartItem: IShoppingCartItem = {
+    const [shoppingCartFruit, setshoppingCartFruit] = useState<IshoppingCartFruitItem[]>([]);
+    const [shoppingCartComplement, setShoppingCartComplement] = useState<shoppingCartComplement[]>([]);
+    const [shoppingCartSizeCup, setShoppingCartSizeCup] = useState<shoppingCartComplement[]>([]);
+
+
+    const handleAddFruit = (id: number) => {
+        // --- Procura pelo id a fruta escolhida
+        const fruit = fruits.find(fruit => fruit.id === id);
+        // Adicionar o Item
+        const cartItem: IshoppingCartFruitItem = {
             produto: fruit!,
-            quantidade: 1,
         };
-        const newShoppingCart: IShoppingCartItem[] = [...shoppingCart, cartItem];
-        setShoppingCart(newShoppingCart);
+        const newshoppingCartFruit: IshoppingCartFruitItem[] = [cartItem];
+        setshoppingCartFruit(newshoppingCartFruit);
     };
 
-    const handleRemoveToCart = (id: number) => {
-        const alreadyInShoppingCart = shoppingCart.find(item => item.produto.id === id);
+    const handleAddComplement = (id: number) => {
+        // --- Procura pelo id a fruta escolhida
+        const complement = complements.find(complement => complement.id === id);
+        // Adicionar o Item
+        const cartItem: shoppingCartComplement = {
+            produto: complement!,
+        };
+        const newshoppingCartComplement: shoppingCartComplement[] = [cartItem];
+        setShoppingCartComplement(newshoppingCartComplement);
+    };
 
-        if (alreadyInShoppingCart!.quantidade > 1) {
-            const newShoppingCart: IShoppingCartItem[] = shoppingCart.map(item => {
+    const handleAddSizeCup = (id: number) => {
+        // --- Procura pelo id a fruta escolhida
+        const sizeCup = sizeCups.find(sizeCup => sizeCup.id === id);
+        // Adicionar o Item
+        const cartItem: shoppingCartComplement = {
+            produto: sizeCup!,
+        };
+        const newshoppingCartSizeCup: shoppingCartComplement[] = [cartItem];
+        setShoppingCartSizeCup(newshoppingCartSizeCup);
+    };
+
+
+
+
+    const handleRemoveToCart = (id: number) => {
+        const alreadyInshoppingCartFruit = shoppingCartFruit.find(item => item.produto.id === id);
+
+        if (alreadyInshoppingCartFruit!.quantidade > 1) {
+            const newshoppingCartFruit: IshoppingCartFruitItem[] = shoppingCartFruit.map(item => {
                 if (item.produto.id === id) ({
                     ...item,
                     quantidade: item.quantidade--,
                 });
                 return item;
             });
-            setShoppingCart(newShoppingCart);
+            setshoppingCartFruit(newshoppingCartFruit);
             return;
         };
 
         // if there is only one item with the id in the cart
-        const newShoppingCart: IShoppingCartItem[] = shoppingCart.filter(item => item.produto.id !== id);
-        setShoppingCart(newShoppingCart);
+        const newshoppingCartFruit: IshoppingCartFruitItem[] = shoppingCartFruit.filter(item => item.produto.id !== id);
+        setshoppingCartFruit(newshoppingCartFruit);
 
     };
+
 
 
     return (
@@ -106,26 +179,81 @@ export default function Preparando() {
 
                 <div className="shadow-lg shadow-pink-500/30 hover:bg-pink-200/20 duration-500 text-slate-500 w-[35rem] h-screen rounded-lg m-2 justify-center items-center flex flex-col">
 
-                    <h1 className="pb-8 text-pink-700">Preparando</h1>
-                    <h2>Frutas:</h2>
 
-                    <ul>
-                        {fruits.map((fruit) => (
+                    {/* Escolhas do Pedido */}
+
+
+                    <h1 className="pb-8 text-pink-700">Escolher</h1>
+                    <div className="flex">
+                        <div className="flex flex-col">
+                            <h2>Frutas:</h2>
+                            {fruits.map((fruit) => (
+                                <button
+                                    className="flex p-2 gap-4"
+                                    key={crypto.randomUUID()}
+                                    onClick={() => handleAddFruit(fruit.id)}>
+                                    {fruit.name}
+                                </button>
+                            ))}
+                        </div>
+
+                        <div className="flex flex-col">
+                            <h2>Recheio:</h2>
+                            {complements.map((complement) => (
+                                <button
+                                    className="flex p-2 gap-4"
+                                    key={crypto.randomUUID()}
+                                    onClick={() => handleAddComplement(complement.id)}>
+                                    {complement.name}
+                                </button>
+                            ))}
+                        </div>
+
+                        <div className="flex flex-col">
+                            <h2>Tamanho:</h2>
+                            {sizeCups.map((sizeCup) => (
+                                <button
+                                    className="flex p-2 gap-4"
+                                    key={crypto.randomUUID()}
+                                    onClick={() => handleAddSizeCup(sizeCup.id)}>
+                                    {sizeCup.name} R${sizeCup.price}
+                                </button>
+                            ))}
+
+                        </div>
+                    </div>
+
+                    {/* Sendo Realizado o Pedido */}
+                    <h1 className="pt-8 text-pink-700">Pedido</h1>
+                    <div className="flex gap-5">
+                        <ul>
+                            {shoppingCartFruit.map((item) => (<p key={crypto.randomUUID()}>{item.produto.name}</p>))}
+                        </ul>
+
+                        <ul>
+                            {shoppingCartComplement.map((item) => (<p key={crypto.randomUUID()}>{item.produto.name}</p>))}
+
+                        </ul>
+
+                        <ul>
+                            {shoppingCartSizeCup.map((item) => (<p key={crypto.randomUUID()}>{item.produto.name} R$ {item.produto.price}</p>))}
+
+                        </ul>
+
+                    </div>
+
+
+
+                    {/* Preparando o Pedido */}
+
+
+
+
+                    {/* ABAIXO É O RESULTADO */}
+                    {/* <ul>
+                        {shoppingCartFruit.map((item) => (
                             <div key={crypto.randomUUID()}>
                                 <li className="flex p-2 gap-4">
-                                    <button onClick={() => handleAddToCart(fruit.id)}>{fruit.name}</button>
-                                </li>
-                            </div>
-                        ))}
-                    </ul>
-
-                    <h1 className="pt-8 text-pink-700">Carrinho</h1>
-
-                    <ul>
-                        {shoppingCart.map((item) => (
-                            <div key={crypto.randomUUID()}>
-                                <li className="flex p-2 gap-4">
-                                    <p>Fruta:{item.produto.name}</p>
                                     <p>Quantos: {item.quantidade}x</p>
                                     <p>Valor: R${item.produto.price},00</p>
                                     <p>Total: R${item.quantidade * item.produto.price},00</p>
@@ -134,20 +262,7 @@ export default function Preparando() {
                                 </li>
                             </div>
                         ))}
-                    </ul>
-                    <ul>
-                        {shoppingCart.map((item) => (
-                            <div key={crypto.randomUUID()}>
-                                <li className="flex p-2 gap-4">
-                                    <p>Quantos: {item.quantidade}x</p>
-                                    <p>Valor: R${item.produto.price},00</p>
-                                    <p>Total: R${item.quantidade * item.produto.price},00</p>
-
-                                    <button onClick={() => handleRemoveToCart(item.produto.id)}>Remove</button>
-                                </li>
-                            </div>
-                        ))}
-                    </ul>
+                    </ul> */}
 
 
                 </div>
