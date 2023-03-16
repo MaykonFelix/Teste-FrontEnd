149b143240c1c85769f6c055c0f6abda2ba866c1
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-03-16 13:24:46 -0300

    Update

--- Client/src/components/API/axioapi.tsx
+++ Client/src/components/API/axioapi.tsx
@@ -2,43 +2,44 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 
+export const axiosapi = () => {
 
-// Pegando API ----------------------
-const [urlFruit, setUrlFrui] = useState('https://mock-test-virid.vercel.app/fruits');
-const [urlSizeCup, setUrlSizeCup] = useState('https://mock-test-virid.vercel.app/sizecup');
-const [urlComplement, setUrlComplement] = useState('https://mock-test-virid.vercel.app/complements');
-// Pegando API ----------------------
+    // Pegando API ----------------------
+    const [urlFruit, setUrlFrui] = useState('https://mock-test-virid.vercel.app/fruits');
+    const [urlSizeCup, setUrlSizeCup] = useState('https://mock-test-virid.vercel.app/sizecup');
+    const [urlComplement, setUrlComplement] = useState('https://mock-test-virid.vercel.app/complements');
+    // Pegando API ----------------------
 
-// Manipulando API ----------------------
-const [postsFruit, setPostsFruit] = useState([])
-const [postsSizeCup, setPostsSizeCup] = useState([])
-const [postsComplement, setPostsComplement] = useState([])
-// Manipulando API ----------------------
+    // Manipulando API ----------------------
+    const [postsFruit, setPostsFruit] = useState([])
+    const [postsSizeCup, setPostsSizeCup] = useState([])
+    const [postsComplement, setPostsComplement] = useState([])
+    // Manipulando API ----------------------
 
 
-//Função para carregar API ----------------------
-const getPosts = async () => {
-    try {
-        const resFruit = await axios.get(urlFruit);
-        var { data } = resFruit;
-        setPostsFruit(data);
+    //Função para carregar API ----------------------
+    const getPosts = async () => {
+        try {
+            const resFruit = await axios.get(urlFruit);
+            var { data } = resFruit;
+            setPostsFruit(data);
 
-        const resSizeCup = await axios.get(urlSizeCup);
-        var { data } = resSizeCup;
-        setPostsSizeCup(data);
+            const resSizeCup = await axios.get(urlSizeCup);
+            var { data } = resSizeCup;
+            setPostsSizeCup(data);
 
-        const resComplement = await axios.get(urlComplement);
-        var { data } = resComplement;
-        setPostsComplement(data);
+            const resComplement = await axios.get(urlComplement);
+            var { data } = resComplement;
+            setPostsComplement(data);
 
-    } catch (error) {
-        console.log(error)
+        } catch (error) {
+            console.log(error)
+        };
     };
-};
-useEffect(() => {
-    getPosts()
-}, [])
-//Função da API ----------------------
+    useEffect(() => {
+        getPosts()
+    }, [])
+    //Função da API ----------------------
 
 
-export { postsFruit, postsSizeCup, postsComplement };
+}
